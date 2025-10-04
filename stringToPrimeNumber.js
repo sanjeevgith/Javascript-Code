cee421ad68645c8691c42a93bb92a3a5ef997960
sanjeevgith <prajapatisanjiv8@gmail.com.com>
2025-10-04 15:32:21 +0530

    string to prime number

---
+++ stringToPrimeNumber.js
@@ -0,0 +1,24 @@
+//convert array string into randum prime number 
+let arrayString = [1, 2, 4, 'san', 3, 'ajay', 'e', 4];
+function isPrime(num) {
+    if (num < 1 || num == 1) {
+        return false;
+    }
+    for (let i = 2; i < num; i++) {
+        if (num % 2 == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+let primeCountNum = 1;
+for (let i = 0; i < arrayString.length; i++) {
+    if (typeof arrayString[i] == 'string') {
+        if (!isPrime(primeCountNum)) {
+            primeCountNum++
+        }
+        arrayString[i] = primeCountNum;
+        primeCountNum++
+    }
+}
+console.log("arrayString", arrayString);
