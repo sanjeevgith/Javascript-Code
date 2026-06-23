ca04887cd32cf67665dd4c655de9260d361f3a3d
sanjeevgith <prajapatisanjiv8@gmail.com.com>
2026-06-23 15:02:09 +0530

    description added

--- removeEnteredNumFromArray.js
+++ removeEnteredNumFromArray.js
@@ -1,11 +1,13 @@
 let dataRemove = [1, 2, 3, 4, 5];
 let numRemove = 4;
 
-let index = data.indexOf(numRemove);
+let index = dataRemove.indexOf(numRemove);
 
 // If the element is found, remove it
-if (index !== -1) {
-    data.splice(index, 1); //splice remove index value
+if (index !== -1) { //if element in not present then indexOf return -1 we are using this condition to check if element is present or not
+    //!== because we want to check if index is not equal to -1, which means the element is present in the array. 
+    // If it is present, we can remove it using splice method.
+    dataRemove.splice(index, 1); //splice remove index value
 }
 
-console.log(data); // Output: [1, 2, 3, 5]
+console.log(dataRemove); // Output: [1, 2, 3, 5]
